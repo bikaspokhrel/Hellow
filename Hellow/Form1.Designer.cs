a8823422cc5271ed76abe6b270166ae9fbb349f0
Bikash Pokhrel <bpokhrel@aut.ac.nz>
2021-02-23 14:33:21 +1300

    Add project files.

---
+++ Hellow/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Hellow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.lbxHello = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnClickMe.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnClickMe.BackgroundImage")));
+            this.btnClickMe.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnClickMe.Location = new System.Drawing.Point(304, 253);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(208, 83);
+            this.btnClickMe.TabIndex = 0;
+            this.btnClickMe.Text = "Click Me";
+            this.btnClickMe.UseVisualStyleBackColor = false;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // lbxHello
+            // 
+            this.lbxHello.FormattingEnabled = true;
+            this.lbxHello.ItemHeight = 16;
+            this.lbxHello.Location = new System.Drawing.Point(533, 122);
+            this.lbxHello.Name = "lbxHello";
+            this.lbxHello.Size = new System.Drawing.Size(255, 244);
+            this.lbxHello.TabIndex = 1;
+            this.lbxHello.SelectedIndexChanged += new System.EventHandler(this.lbxHello_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Heading", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(274, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "WELCOME TO THE LEVEL 6";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblOutput.Location = new System.Drawing.Point(299, 122);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(211, 56);
+            this.lblOutput.TabIndex = 4;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(314, 207);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(176, 22);
+            this.txtInput.TabIndex = 5;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(316, 342);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(196, 52);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear Listbox";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Your Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxHello);
+            this.Controls.Add(this.btnClickMe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.ListBox lbxHello;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Hellow/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hellow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, EventArgs e)
+        {
+            //lblOutput.Text = "Hellow World";
+            //lblOutput.Text = txtInput.Text; //this will outuput the text input by users in the textbox
+            //lbxHello.Items.Add(txtInput.Text);//this will add items from the textbox to the list box
+            string Country = "New Zealand";
+            string Name = txtInput.Text;
+            lbxHello.Items.Add(Country);
+            MessageBox.Show("Hi My Name is " +Name+ ", I came from " + Country + ", which is a beautiful nation");
+        }
+        
+        private void lbxHello_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbxHello.Items.Clear();
+        }
+    }
+}
